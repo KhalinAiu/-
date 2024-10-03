03f3399446f1513fc14e26b06d49a7a3eef911b1
KhalinAiu <khalin.aiu@phystech.edu>
2024-10-03 19:40:15 +0300

    Add files via upload

--- Упр_3_на _04.10.py
+++ Упр_3_на _04.10.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 x = ['Белуга', 'Калуга', 'Таймень', 'Севрюга', 'Сазан']
 y = [2000, 1000, 80, 70, 40]
 plt.bar(x, y, alpha=0.5 )
-plt.plot(x, y, color='purple', marker='o', markersize=7)
+plt.plot(x, y, color='green', marker='s', markersize=5)
 plt.xlabel('Вид рыбы')
 plt.ylabel('Масса в кг')
 plt.title('Максимальная масса крупнейших рыб Росии')
